9a96597f63c467ac7b93c229b6817b2e541527f2
William Ughetta <wughetta@princeton.edu>
2021-03-09 12:57:06 -0500

    Add code2str from code.h

--- code.go
+++ code.go
@@ -19,10 +19,41 @@ const VERB	= (V_IRREG|ED)
 const MAN	= (1<<11) /* +man, +men, +women, +woman */
 const ADV	= (1<<12) /* +hood, +ness */
 const STOP	= (1<<14) /* stop list */
-
 const NOPREF	= (1<<13) /* no prefix */
+
 const MONO	= (1<<15) /* double final consonant as in fib->fibbing */
 const IN	= (1<<16) /* in- im- ir, not un- */
 const _Y	= (1<<17) /* +y */
 
 const ALL	= (~(NOPREF|STOP|DONT_TOUCH|MONO|IN)) /*anything goes (no stop or nopref)*/
+
+codeNames := map[int]string {
+	ED:	"ED",
+	ADJ:	"AD",
+	NOUN:	"NOUN",
+	PROP_COLLECT: "PROP_COLLECT",
+	ACTOR:	"ACTOR",
+	EST:	"EST",
+	DONT_TOUCH: "DONT_TOUCH",
+	ION:	"ION",
+	N_AFFIX: "N_AFFIX",
+	V_AFFIX: "V_AFFIX",
+	V_IRREG: "V_IRREG",
+	MAN:	"MAN",
+	ADV:	"ADV",
+	STOP:	"STOP",
+	NOPREF:	"NOPREF",
+	MONO:	"MONO",
+	IN:	"IN",
+	_Y:	"_Y",
+}
+
+func codeToString(code int) string {
+	buf := ""
+	for k, v := range codeNames {
+		if code & k != 0 {
+			buf += "|" + v
+		}
+	}
+	return buf[1:] // Strip leading '|'
+}
